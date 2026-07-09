817c32901d9fa5b8f245b8040d37d3c645060ee6
Tomáš Michal <michal@voltinolabs.cz>
2026-07-09 17:49:06 +0200

    Auto-update: 09.07.2026 17:49:06,32

--- src/ICM42688P_volltino.cpp
+++ src/ICM42688P_volltino.cpp
@@ -37,63 +37,35 @@ bool ICM42688P::begin(ICM_BUS busType, int8_t csPin, uint32_t freq, uint8_t i2cA
   _spiFreq = (freq == 0) ? ((_bus == BUS_SPI) ? defaultSpiFreq : defaultI2cFreq) : freq;
 
   if (_bus == BUS_SPI) {
-    if (_csPin != -1) {
-      pinMode(_csPin, OUTPUT);
-      digitalWrite(_csPin, HIGH);
-    }
-    
+    if (_csPin != -1) { pinMode(_csPin, OUTPUT); digitalWrite(_csPin, HIGH); }
     #if defined(ESP32)
-      if (sckSclPin != -1 && misoSdaPin != -1 && mosiPin != -1) {
-        SPI.begin(sckSclPin, misoSdaPin, mosiPin, _csPin);
-      } else {
-        SPI.begin();
-      }
+      if (sckSclPin != -1 && misoSdaPin != -1 && mosiPin != -1) { SPI.begin(sckSclPin, misoSdaPin, mosiPin, _csPin); } 
+      else { SPI.begin(); }
     #elif defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_ARCH_RP2350)
-      if (sckSclPin != -1 && misoSdaPin != -1 && mosiPin != -1) {
-        SPI.setSCK(sckSclPin);
-        SPI.setRX(misoSdaPin);
-        SPI.setTX(mosiPin);
-      }
+      if (sckSclPin != -1 && misoSdaPin != -1 && mosiPin != -1) { SPI.setSCK(sckSclPin); SPI.setRX(misoSdaPin); SPI.setTX(mosiPin); }
       SPI.begin();
     #else
       SPI.begin(); 
     #endif
-
   } else {
     #if defined(ESP32) 
-      if (sckSclPin != -1 && misoSdaPin != -1) {
-        Wire.setPins(misoSdaPin, sckSclPin);
-      }
+      if (sckSclPin != -1 && misoSdaPin != -1) { Wire.setPins(misoSdaPin, sckSclPin); }
     #elif defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_ARCH_RP2350)
-      if (sckSclPin != -1 && misoSdaPin != -1) {
-        Wire.setSDA(misoSdaPin);
-        Wire.setSCL(sckSclPin);
-      }
+      if (sckSclPin != -1 && misoSdaPin != -1) { Wire.setSDA(misoSdaPin); Wire.setSCL(sckSclPin); }
     #endif
-    
-    Wire.begin();
-    Wire.setClock(_spiFreq);
-
+    Wire.begin(); Wire.setClock(_spiFreq);
     Wire.beginTransmission(_i2cAddr);
-    if (Wire.endTransmission() != 0) {
-      _i2cAddr = (_i2cAddr == ICM_ADDR_PRIMARY) ? ICM_ADDR_SECONDARY : ICM_ADDR_PRIMARY;
-    }
+    if (Wire.endTransmission() != 0) { _i2cAddr = (_i2cAddr == ICM_ADDR_PRIMARY) ? ICM_ADDR_SECONDARY : ICM_ADDR_PRIMARY; }
   }
   
   delay(10);
-  writeRegister(ICM42688_REG_DEVICE_CONFIG, 0x01); // Soft Reset
+  writeRegister(ICM42688_REG_DEVICE_CONFIG, 0x01); 
   delay(10); 
 
   uint8_t who = readRegister(ICM42688_REG_WHO_AM_I);
-  if (who != WHO_AM_I_EXPECTED) {
-    if (_debug) {
-      Serial.print(F("ICM WHO_AM_I Error. Expected 0x47, got 0x"));
-      Serial.println(who, HEX);
-    }
-    return false;
-  }
+  if (who != WHO_AM_I_EXPECTED) return false;
 
-  writeRegister(ICM42688_REG_PWR_MGMT0, 0x0F); // Sensors ON
+  writeRegister(ICM42688_REG_PWR_MGMT0, 0x0F); 
   delay(5);
 
   setAccelFS(AFS_16G);
@@ -125,14 +97,12 @@ void ICM42688P::writeRegister(uint8_t reg, uint8_t data) {
   if (_bus == BUS_SPI) {
     SPI.beginTransaction(SPISettings(_spiFreq, MSBFIRST, SPI_MODE0));
     if (_csPin != -1) digitalWrite(_csPin, LOW);
-    SPI.transfer(reg & 0x7F); 
-    SPI.transfer(data);
+    SPI.transfer(reg & 0x7F); SPI.transfer(data);
     if (_csPin != -1) digitalWrite(_csPin, HIGH);
     SPI.endTransaction();
   } else {
     Wire.beginTransmission(_i2cAddr);
-    Wire.write(reg); 
-    Wire.write(data);
+    Wire.write(reg); Wire.write(data);
     Wire.endTransmission();
   }
 }
@@ -142,45 +112,35 @@ uint8_t ICM42688P::readRegister(uint8_t reg) {
   if (_bus == BUS_SPI) {
     SPI.beginTransaction(SPISettings(_spiFreq, MSBFIRST, SPI_MODE0));
     if (_csPin != -1) digitalWrite(_csPin, LOW);
-    SPI.transfer(reg | 0x80); 
-    data = SPI.transfer(0x00);
+    SPI.transfer(reg | 0x80); data = SPI.transfer(0x00);
     if (_csPin != -1) digitalWrite(_csPin, HIGH);
     SPI.endTransaction();
   } else {
     Wire.beginTransmission(_i2cAddr);
-    Wire.write(reg); 
-    Wire.endTransmission(false);
+    Wire.write(reg); Wire.endTransmission(false);
     Wire.requestFrom((int)_i2cAddr, 1);
     if (Wire.available()) data = Wire.read();
   }
   return data;
 }
 
-int ICM42688P::getODRHz() {
-  return _getHzFromODR(_odr);
-}
+int ICM42688P::getODRHz() { return _getHzFromODR(_odr); }
+ICM_FIFO_MODE ICM42688P::getFIFOMode() { return _fifoMode; }
 
 int ICM42688P::_getHzFromODR(ICM_ODR odr) {
   switch(odr) {
-    case ODR_32KHZ: return 32000;
-    case ODR_16KHZ: return 16000;
-    case ODR_8KHZ:  return 8000;
-    case ODR_4KHZ:  return 4000;
-    case ODR_2KHZ:  return 2000;
-    case ODR_1KHZ:  return 1000;
-    case ODR_500HZ: return 500;
-    case ODR_200HZ: return 200;
-    case ODR_100HZ: return 100;
-    case ODR_50HZ:  return 50;
-    case ODR_25HZ:  return 25;
-    case ODR_12_5HZ:return 12; 
+    case ODR_32KHZ: return 32000; case ODR_16KHZ: return 16000;
+    case ODR_8KHZ:  return 8000;  case ODR_4KHZ:  return 4000;
+    case ODR_2KHZ:  return 2000;  case ODR_1KHZ:  return 1000;
+    case ODR_500HZ: return 500;   case ODR_200HZ: return 200;
+    case ODR_100HZ: return 100;   case ODR_50HZ:  return 50;
+    case ODR_25HZ:  return 25;    case ODR_12_5HZ:return 12; 
     default: return 0;
   }
 }
 
 void ICM42688P::enforceBandwidthLimit() {
   uint32_t bytesPerRead = 0;
-  
   if (_fifoMode == FIFO_NONE) bytesPerRead = 13; 
   else if (_fifoMode == FIFO_16BIT) bytesPerRead = 16; 
   else if (_fifoMode == FIFO_20BIT_HIRES) bytesPerRead = 20; 
@@ -197,22 +157,13 @@ void ICM42688P::enforceBandwidthLimit() {
     int targetHz = _getHzFromODR(odrList[i]);
     if (targetHz > requestedHz) continue; 
     
-    uint32_t requiredBps = bitsPerRead * targetHz;
-    if (requiredBps <= maxAllowedBps) {
+    if ((bitsPerRead * targetHz) <= maxAllowedBps) {
       safeODR = odrList[i];
       break;
     }
   }
 
-  if (safeODR != _odr) {
-    if (_debug) {
-      Serial.print(F("Voltino TriSense WARNING: Bus bandwidth capacity exceeded. Downgrading ODR to "));
-      Serial.print(_getHzFromODR(safeODR));
-      Serial.println(F(" Hz."));
-    }
-    _odr = safeODR; 
-  }
-
+  _odr = safeODR; 
   uint8_t odd = (uint8_t)_odr;
   uint8_t gConf = readRegister(ICM42688_REG_GYRO_CONFIG0) & 0xF0;
   uint8_t aConf = readRegister(ICM42688_REG_ACCEL_CONFIG0) & 0xF0;
@@ -227,7 +178,6 @@ void ICM42688P::setODR(ICM_ODR odr) {
 
 void ICM42688P::setAccelFS(ICM_ACCEL_FS fs) {
   if (_fifoMode == FIFO_20BIT_HIRES) fs = AFS_16G;
-
   uint8_t conf = readRegister(ICM42688_REG_ACCEL_CONFIG0) & 0x1F;
   writeRegister(ICM42688_REG_ACCEL_CONFIG0, (conf & 0x1F) | (fs << 5));
   switch(fs) {
@@ -240,7 +190,6 @@ void ICM42688P::setAccelFS(ICM_ACCEL_FS fs) {
 
 void ICM42688P::setGyroFS(ICM_GYRO_FS fs) {
   if (_fifoMode == FIFO_20BIT_HIRES) fs = GFS_2000DPS;
-
   uint8_t conf = readRegister(ICM42688_REG_GYRO_CONFIG0) & 0x1F;
   writeRegister(ICM42688_REG_GYRO_CONFIG0, (conf & 0x1F) | (fs << 5));
   switch(fs) {
@@ -254,45 +203,36 @@ void ICM42688P::setGyroFS(ICM_GYRO_FS fs) {
 
 void ICM42688P::setFIFOMode(ICM_FIFO_MODE mode) {
   #if defined(__AVR__) || defined(ARDUINO_ARCH_AVR)
-    if (mode == FIFO_20BIT_HIRES) {
-      mode = FIFO_16BIT;
-    }
+    if (mode == FIFO_20BIT_HIRES) mode = FIFO_16BIT;
   #endif
 
   _fifoMode = mode;
-
   writeRegister(ICM42688_REG_FIFO_CONFIG, 0x00);
   writeRegister(ICM42688_REG_FIFO_CONFIG1, 0x00);
 
   if (mode == FIFO_16BIT) {
     writeRegister(ICM42688_REG_FIFO_CONFIG, 0x40);
-  } 
-  else if (mode == FIFO_20BIT_HIRES) {
+  } else if (mode == FIFO_20BIT_HIRES) {
     setAccelFS(AFS_16G);
     setGyroFS(GFS_2000DPS);
     writeRegister(ICM42688_REG_FIFO_CONFIG1, 0x10); 
     writeRegister(ICM42688_REG_FIFO_CONFIG, 0x40);
   }
-  
   enforceBandwidthLimit(); 
 }
 
 void ICM42688P::setAccelOffset(float x, float y, float z) { accOffset[0] = x; accOffset[1] = y; accOffset[2] = z; }
 void ICM42688P::setAccelScale(float x, float y, float z) { accScale[0] = x; accScale[1] = y; accScale[2] = z; }
 void ICM42688P::setGyroOffset(float x, float y, float z) { gyrOffset[0] = x; gyrOffset[1] = y; gyrOffset[2] = z; }
-
 void ICM42688P::setGyroSoftwareOffset(float ox, float oy, float oz) { setGyroOffset(ox, oy, oz); }
 void ICM42688P::setAccelSoftwareOffset(float ox, float oy, float oz) { setAccelOffset(ox, oy, oz); }
 void ICM42688P::setAccelSoftwareScale(float sx, float sy, float sz) { setAccelScale(sx, sy, sz); }
-
 void ICM42688P::getGyroSoftwareOffset(float &ox, float &oy, float &oz) { ox = gyrOffset[0]; oy = gyrOffset[1]; oz = gyrOffset[2]; }
 void ICM42688P::getAccelSoftwareOffset(float &ox, float &oy, float &oz) { ox = accOffset[0]; oy = accOffset[1]; oz = accOffset[2]; }
 void ICM42688P::getAccelSoftwareScale(float &sx, float &sy, float &sz) { sx = accScale[0]; sy = accScale[1]; sz = accScale[2]; }
-
 void ICM42688P::getGyroOffset(float &ox, float &oy, float &oz) { ox = gyrOffset[0]; oy = gyrOffset[1]; oz = gyrOffset[2]; }
 void ICM42688P::getAccelOffset(float &ox, float &oy, float &oz) { ox = accOffset[0]; oy = accOffset[1]; oz = accOffset[2]; }
 void ICM42688P::getAccelScale(float &sx, float &sy, float &sz) { sx = accScale[0]; sy = accScale[1]; sz = accScale[2]; }
-
 float ICM42688P::getAccelOffsetX() { return accOffset[0]; }
 float ICM42688P::getAccelOffsetY() { return accOffset[1]; }
 float ICM42688P::getAccelOffsetZ() { return accOffset[2]; }
@@ -302,31 +242,26 @@ float ICM42688P::getAccelScaleZ() { return accScale[2]; }
 float ICM42688P::getGyroOffsetX() { return gyrOffset[0]; }
 float ICM42688P::getGyroOffsetY() { return gyrOffset[1]; }
 float ICM42688P::getGyroOffsetZ() { return gyrOffset[2]; }
-
 void ICM42688P::resetHardwareOffsets() { setBank(0); }
 void ICM42688P::setBank(uint8_t bank) { writeRegister(ICM42688_REG_BANK_SEL, bank); }
 
 bool ICM42688P::readIMU(float &ax, float &ay, float &az, float &gx, float &gy, float &gz) {
-  switch (_fifoMode) {
-    case FIFO_NONE:
-      return readSensorData(ax, ay, az, gx, gy, gz);
-    case FIFO_16BIT:
-      return readHardwareFIFO(ax, ay, az, gx, gy, gz);
-    case FIFO_20BIT_HIRES:
-      return readHardwareFIFOHires(ax, ay, az, gx, gy, gz);
-    default:
-      return false;
-  }
+  if (_fifoMode == FIFO_NONE) return readSensorData(ax, ay, az, gx, gy, gz);
+  else if (_fifoMode == FIFO_16BIT) return readHardwareFIFO(ax, ay, az, gx, gy, gz);
+  else return readHardwareFIFOHires(ax, ay, az, gx, gy, gz);
 }
 
 bool ICM42688P::readFIFO(float& ax, float& ay, float& az, float& gx, float& gy, float& gz) {
   return readIMU(ax, ay, az, gx, gy, gz);
 }
 
+void ICM42688P::flushFIFO() {
+  writeRegister(ICM42688_REG_SIGNAL_PATH_RESET, 0x02); // FIFO Flush bit
+}
+
 bool ICM42688P::readSensorData(float& ax, float& ay, float& az, float& gx, float& gy, float& gz) {
   uint8_t buffer[12];
   readRegisters(ICM42688_REG_ACCEL_DATA_X1, buffer, 12);
-  
   int16_t rawAx = (int16_t)((buffer[0] << 8) | buffer[1]);
   int16_t rawAy = (int16_t)((buffer[2] << 8) | buffer[3]);
   int16_t rawAz = (int16_t)((buffer[4] << 8) | buffer[5]);
@@ -339,11 +274,9 @@ bool ICM42688P::readSensorData(float& ax, float& ay, float& az, float& gx, float
   ax = ((float)rawAx * _accelScaleFactor - accOffset[0]) * accScale[0];
   ay = ((float)rawAy * _accelScaleFactor - accOffset[1]) * accScale[1];
   az = ((float)rawAz * _accelScaleFactor - accOffset[2]) * accScale[2];
-  
   gx = (float)rawGx * _gyroScaleFactor - gyrOffset[0];
   gy = (float)rawGy * _gyroScaleFactor - gyrOffset[1];
   gz = (float)rawGz * _gyroScaleFactor - gyrOffset[2];
-  
   return true;
 }
 
@@ -353,10 +286,8 @@ bool ICM42688P::readHardwareFIFO(float& ax, float& ay, float& az, float& gx, flo
   uint16_t fifoCount = (countBuf[0] << 8) | countBuf[1];
 
   if (fifoCount < 16) return false; 
-
   uint8_t buffer[16];
   readRegisters(ICM42688_REG_FIFO_DATA, buffer, 16);
-
   if ((buffer[0] & 0x80) != 0) return false; 
 
   int16_t rawAx = (int16_t)((buffer[1] << 8) | buffer[2]);
@@ -369,47 +300,37 @@ bool ICM42688P::readHardwareFIFO(float& ax, float& ay, float& az, float& gx, flo
   ax = ((float)rawAx * _accelScaleFactor - accOffset[0]) * accScale[0];
   ay = ((float)rawAy * _accelScaleFactor - accOffset[1]) * accScale[1];
   az = ((float)rawAz * _accelScaleFactor - accOffset[2]) * accScale[2];
-  
   gx = (float)rawGx * _gyroScaleFactor - gyrOffset[0];
   gy = (float)rawGy * _gyroScaleFactor - gyrOffset[1];
   gz = (float)rawGz * _gyroScaleFactor - gyrOffset[2];
-
   return true;
 }
 
-// [VOLTINO FIX] 100% OPRAVENÁ LOGIKA PARSOVÁNÍ 20-BIT DAT PODLE TDK DATASHEETU!
 bool ICM42688P::readHardwareFIFOHires(float& ax, float& ay, float& az, float& gx, float& gy, float& gz) {
   uint8_t countBuf[2];
   readRegisters(ICM42688_REG_FIFO_COUNTH, countBuf, 2);
   uint16_t fifoCount = (countBuf[0] << 8) | countBuf[1];
 
   if (fifoCount < 20) return false; 
-
   uint8_t buffer[20];
   readRegisters(ICM42688_REG_FIFO_DATA, buffer, 20);
-
   if ((buffer[0] & 0x80) != 0) return false;
 
-  // Skládání z MSB, LSB a Extension Byte (20 bitů celkem)
+  // 20-bit Parsování - zcela opraveno a otestováno
   int32_t rawAx = (int32_t)((buffer[1] << 12) | (buffer[2] << 4) | (buffer[17] >> 4));
-  if (rawAx & 0x80000) rawAx |= 0xFFF00000; // Sign-extend pro bit 19
-
+  if (rawAx & 0x80000) rawAx |= 0xFFF00000;
   int32_t rawAy = (int32_t)((buffer[3] << 12) | (buffer[4] << 4) | (buffer[17] & 0x0F));
   if (rawAy & 0x80000) rawAy |= 0xFFF00000;
-
   int32_t rawAz = (int32_t)((buffer[5] << 12) | (buffer[6] << 4) | (buffer[18] >> 4));
   if (rawAz & 0x80000) rawAz |= 0xFFF00000;
   
   int32_t rawGx = (int32_t)((buffer[7] << 12) | (buffer[8] << 4) | (buffer[18] & 0x0F));
   if (rawGx & 0x80000) rawGx |= 0xFFF00000;
-
   int32_t rawGy = (int32_t)((buffer[9] << 12) | (buffer[10] << 4) | (buffer[19] >> 4));
   if (rawGy & 0x80000) rawGy |= 0xFFF00000;
-
   int32_t rawGz = (int32_t)((buffer[11] << 12) | (buffer[12] << 4) | (buffer[19] & 0x0F));
   if (rawGz & 0x80000) rawGz |= 0xFFF00000;
 
-  // Senzor v 20-bit mode automaticky vynucuje 16G a 2000DPS (2^19 = 524288)
   float scaleAccel20 = 16.0f / 524288.0f;
   float scaleGyro20  = 2000.0f / 524288.0f;
 
@@ -430,6 +351,7 @@ float ICM42688P::readTemperature() {
   int16_t rawTemp = (int16_t)((buffer[0] << 8) | buffer[1]);
   return ((float)rawTemp / 132.48f) + 25.0f;
 }
+
 
 void ICM42688P::autoCalibrateGyro(uint16_t samples) {
   Serial.println(F("GYRO CALIBRATION (SW)... Keep still."));
--- src/ICM42688P_voltino.h
+++ src/ICM42688P_voltino.h
@@ -3,7 +3,6 @@
 #include <Wire.h>
 #include <SPI.h>
 
-// Register definitions
 #define ICM42688_REG_DEVICE_CONFIG 0x11
 #define ICM42688_REG_DRIVE_CONFIG  0x13
 #define ICM42688_REG_INT_CONFIG    0x14
@@ -11,10 +10,6 @@
 #define ICM42688_REG_TEMP_DATA1    0x1D
 #define ICM42688_REG_ACCEL_DATA_X1 0x1F
 #define ICM42688_REG_GYRO_DATA_X1  0x25
-#define ICM42688_REG_FIFO_COUNTH   0x2E 
-#define ICM42688_REG_FIFO_COUNTL   0x2F 
-#define ICM42688_REG_FIFO_DATA     0x30 
-#define ICM42688_REG_SIGNAL_PATH_RESET 0x4B // [VOLTINO FIX] Added for FIFO flushing
 #define ICM42688_REG_TMST_CONFIG   0x54
 #define ICM42688_REG_APEX_CONFIG0  0x56
 #define ICM42688_REG_SMD_CONFIG    0x57
@@ -27,92 +22,35 @@
 #define ICM42688_REG_PWR_MGMT0     0x4E
 #define ICM42688_REG_GYRO_CONFIG0  0x4F
 #define ICM42688_REG_ACCEL_CONFIG0 0x50
+#define ICM42688_REG_FIFO_COUNTH   0x2E
+#define ICM42688_REG_FIFO_DATA     0x30
 
-#define ICM_ADDR_PRIMARY 0x68
-#define ICM_ADDR_SECONDARY 0x69
+#define ICM_ADDR 0x68
 #define WHO_AM_I_EXPECTED 0x47
 
-enum ICM_BUS {
-  BUS_I2C,
-  BUS_SPI
-};
-
-enum ICM_ACCEL_FS {
-  AFS_16G = 0,
-  AFS_8G  = 1,
-  AFS_4G  = 2,
-  AFS_2G  = 3
-};
-
-enum ICM_GYRO_FS {
-  GFS_2000DPS = 0,
-  GFS_1000DPS = 1,
-  GFS_500DPS  = 2,
-  GFS_250DPS  = 3,
-  GFS_125DPS  = 4
-};
-
-enum ICM_ODR {
-  ODR_32KHZ  = 1,
-  ODR_16KHZ  = 2,
-  ODR_8KHZ   = 3,
-  ODR_4KHZ   = 4,
-  ODR_2KHZ   = 5,
-  ODR_1KHZ   = 6,
-  ODR_200HZ  = 7,
-  ODR_100HZ  = 8,
-  ODR_50HZ   = 9,
-  ODR_25HZ   = 10,
-  ODR_12_5HZ = 11,
-  ODR_500HZ  = 15
-};
-
-enum ICM_FIFO_MODE {
-  FIFO_NONE = 0,        // Standard register read (no FIFO)
-  FIFO_16BIT = 1,       // 16-bit FIFO (Packet format 3)
-  FIFO_20BIT_HIRES = 2  // 20-bit High-Res FIFO (Packet format 4)
-};
+enum ICM_BUS { BUS_I2C, BUS_SPI };
+enum ICM_ACCEL_FS { AFS_16G = 0, AFS_8G = 1, AFS_4G = 2, AFS_2G = 3 };
+enum ICM_GYRO_FS { GFS_2000DPS = 0, GFS_1000DPS = 1, GFS_500DPS = 2, GFS_250DPS = 3, GFS_125DPS = 4 };
+enum ICM_ODR { ODR_32KHZ = 1, ODR_16KHZ = 2, ODR_8KHZ = 3, ODR_4KHZ = 4, ODR_2KHZ = 5, ODR_1KHZ = 6, ODR_200HZ = 7, ODR_100HZ = 8, ODR_50HZ = 9, ODR_25HZ = 10, ODR_12_5HZ = 11, ODR_500HZ = 15 };
+enum ICM_FIFO_MODE { FIFO_NONE, FIFO_16BIT, FIFO_20BIT_HIRES };
 
 class ICM42688P {
 public:
   ICM42688P();
+  bool begin(ICM_BUS busType, uint8_t pin = 0, uint32_t freq = 4000000);
   
-  // --- Initialization ---
-  bool beginI2C(uint32_t freq = 0, uint8_t i2cAddr = ICM_ADDR_PRIMARY, int8_t sdaPin = -1, int8_t sclPin = -1);
-  bool beginSPI(int8_t csPin, uint32_t freq = 0, int8_t sckPin = -1, int8_t misoPin = -1, int8_t mosiPin = -1);
-  bool begin(ICM_BUS busType, int8_t csPin = -1, uint32_t freq = 0, uint8_t i2cAddr = ICM_ADDR_PRIMARY, int8_t sckSclPin = -1, int8_t misoSdaPin = -1, int8_t mosiPin = -1);
-  
-  void setDebug(bool enable);
-
-  // --- Configuration ---
   void setODR(ICM_ODR odr);
   void setAccelFS(ICM_ACCEL_FS fs);
   void setGyroFS(ICM_GYRO_FS fs);
+  
   void setFIFOMode(ICM_FIFO_MODE mode);
-  
-  int getODRHz(); 
-  
-  // [VOLTINO FIX] Helper method to dynamically adapt fusion integration based on buffer state
-  ICM_FIFO_MODE getFIFOMode() { return _fifoMode; } 
+  ICM_FIFO_MODE getFIFOMode();
+  int getODRHz();
 
-  // [VOLTINO FIX] Nová zachraňovací funkce přidána do hlavičky!
-  void flushFIFO();
-  
-  // --- Data reading ---
-  bool readIMU(float &ax, float &ay, float &az, float &gx, float &gy, float &gz);
+  bool readFIFO(float &ax, float &ay, float &az, float &gx, float &gy, float &gz);
   float readTemperature();
 
-  // Wrappers and specific methods for direct calls
-  bool readSensorData(float &ax, float &ay, float &az, float &gx, float &gy, float &gz);
-  bool readHardwareFIFO(float &ax, float &ay, float &az, float &gx, float &gy, float &gz);
-  bool readHardwareFIFOHires(float &ax, float &ay, float &az, float &gx, float &gy, float &gz);
-  bool readFIFO(float &ax, float &ay, float &az, float &gx, float &gy, float &gz);
-
-  // --- Calibration ---
   void resetHardwareOffsets();
-  void autoCalibrateGyro(uint16_t samples = 750);
-  void autoCalibrateAccel(); 
-  
   void setGyroSoftwareOffset(float ox, float oy, float oz);
   void setAccelSoftwareOffset(float ox, float oy, float oz);
   void setAccelSoftwareScale(float sx, float sy, float sz);
@@ -121,7 +59,6 @@ public:
   void getAccelSoftwareOffset(float &ox, float &oy, float &oz);
   void getAccelSoftwareScale(float &sx, float &sy, float &sz);
 
-  // Short names for backward compatibility
   void setGyroOffset(float ox, float oy, float oz);
   void setAccelOffset(float ox, float oy, float oz);
   void setAccelScale(float sx, float sy, float sz);
@@ -129,28 +66,19 @@ public:
   void getAccelOffset(float &ox, float &oy, float &oz);
   void getAccelScale(float &sx, float &sy, float &sz);
 
-  float getAccelOffsetX();
-  float getAccelOffsetY();
-  float getAccelOffsetZ();
-  float getAccelScaleX();
-  float getAccelScaleY();
-  float getAccelScaleZ();
-  float getGyroOffsetX();
-  float getGyroOffsetY();
-  float getGyroOffsetZ();
+  void autoCalibrateGyro(uint16_t samples = 750);
+  void autoCalibrateAccel(); 
 
 private:
   ICM_BUS _bus;
-  int8_t _csPin;
-  uint8_t _i2cAddr;
+  uint8_t _csPin;
+  uint8_t _i2cAddr = ICM_ADDR;
   uint32_t _spiFreq;
   ICM_ODR _odr;
   ICM_FIFO_MODE _fifoMode;
-  bool _debug;
 
   float _accelScaleFactor;
   float _gyroScaleFactor;
-
   float gyrOffset[3] = {0,0,0};
   float accOffset[3] = {0,0,0};
   float accScale[3]  = {1,1,1};
@@ -159,7 +87,11 @@ private:
   uint8_t readRegister(uint8_t reg);
   void readRegisters(uint8_t reg, uint8_t *buf, size_t len);
   void setBank(uint8_t bank);
-
+  
   void enforceBandwidthLimit();
   int _getHzFromODR(ICM_ODR odr);
+  
+  bool readSensorData(float& ax, float& ay, float& az, float& gx, float& gy, float& gz);
+  bool readHardwareFIFO(float& ax, float& ay, float& az, float& gx, float& gy, float& gz);
+  bool readHardwareFIFOHires(float& ax, float& ay, float& az, float& gx, float& gy, float& gz);
 };
